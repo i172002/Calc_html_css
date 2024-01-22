e3696233f7811ba9f3b3d63bd8508a5f85c973a6
i172002 <infantmary17@gmail.com>
2024-01-22 16:57:12 +0530

    rectified delete bug and added keyboard events

--- script.js
+++ script.js
@@ -4,34 +4,63 @@ let string  = "";
 buttons.forEach(button => {
   button.addEventListener("click", (event) => {
         try{
-
-        
         if(event.target.innerHTML == '=')
         {
-            string = eval(string);
+            input.value = eval(input.value);
+            //string = input.value;
             
                
         } 
         else if(event.target.innerHTML == 'DEL')
         {
-            string = string.substring(0,string.length-1);
+            
+           // string = string.substring(0,string.length-1);
+            input.value = input.value.substring(0,input.value.length-1);
             
         }
         else if (event.target.innerHTML == 'AC')
         {
-            string = ""; 
+            //string = ""; 
+            input.value = "";
         }
        
         else{
-        string+= event.target.innerHTML;
-        
+       // string+= event.target.innerHTML;
+        input.value += event.target.innerHTML;
         }
-        input.value = string;
+        
     }
     catch(error)
     {
-        console.log("invalid syntax");
+        console.log(error);
         window.alert("Invalid Syntax");
     }
   });
+
 });
+
+document.addEventListener("keydown", (event)=>{
+     if((event.key >= "0" && event.key <= "9" ) || (event.key === "+" || event.key === "-" || event.key === "*" || event.key === "/" ||event.key === "%"))
+     { 
+        input.value+=event.key;
+        
+     }
+     else if (event.key == "Delete")
+     {
+        input.value = input.value.substring(0,input.value.length-1);
+     }
+     else if (event.shiftKey && event.key === "Backspace" )
+     {
+        input.value = "";
+     }
+     else if (event.key === "=" || event.key == "Enter" )
+     {
+        input.value = eval(input.value);
+     }
+    
+ }) 
+
+
+//col span and row span in html tables 
+//span tag
+//display:block,inline
